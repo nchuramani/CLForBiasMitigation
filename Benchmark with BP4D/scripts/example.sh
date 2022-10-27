98e2887f7b611c65577359332da5d0078fb2a493
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2022-10-27 10:29:26 -0400

    Update example.sh

--- Benchmark with BP4D/scripts/example.sh
+++ Benchmark with BP4D/scripts/example.sh
@@ -3,5 +3,10 @@ OUTDIR=outputs/BP4D_GENDER_MAS
 REPEAT=3
 mkdir -p $OUTDIR
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 --category gender  
 
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 --category gender --train_aug
+
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 --category race
+
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 --category race --train_aug
