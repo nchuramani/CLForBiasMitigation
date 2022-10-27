8f0f4b234357d5864a505516e2b50eb2e6b0e87f
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2022-10-27 10:55:11 -0400

    Update example.sh

--- Benchmarkwith RAF-DB/scripts/example.sh
+++ Benchmarkwith RAF-DB/scripts/example.sh
@@ -3,12 +3,12 @@ OUTDIR=outputs/RAF-DB/
 REPEAT=3
 mkdir -p $OUTDIR
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC        --lr 0.0001 --reg_coef 1 10 100         --category gender --train_aug
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC    --lr 0.0001 --reg_coef 1 10 100         --category gender --train_aug
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC        --lr 0.0001 --reg_coef 1 10 100         --category gender 
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC    --lr 0.0001 --reg_coef 1 10 100         --category gender 
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC        --lr 0.0001 --reg_coef 1 10 100         --category race --train_aug
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC    --lr 0.0001 --reg_coef 1 10 100         --category race --train_aug
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC        --lr 0.0001 --reg_coef 1 10 100         --category race 
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --no_class_remap --force_out_dim 7 --schedule 25 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type customization  --agent_name EWC    --lr 0.0001 --reg_coef 1 10 100         --category race 
 
 
