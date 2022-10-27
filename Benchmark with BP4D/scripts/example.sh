c8cdf561cc182327745963657359d1a47fa4d0e3
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2022-10-27 10:28:14 -0400

    Update example.sh

--- Benchmark with BP4D/scripts/example.sh
+++ Benchmark with BP4D/scripts/example.sh
@@ -3,7 +3,5 @@ OUTDIR=outputs/BP4D_GENDER_MAS
 REPEAT=3
 mkdir -p $OUTDIR
 
-python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name MAS   --lr 0.0001 --reg_coef 100        | tee ${OUTDIR}/MAS.log
-
-
+python3 -u ../main.py --gpuid $GPUID --repeat $REPEAT --optimizer Adam  --category gender  --no_class_remap --force_out_dim 12 --schedule 5 --batch_size 24 --model_type custom_cnn --model_name Net  --agent_type regularization --agent_name EWC   --lr 0.0001 --reg_coef 1 10 100 
 
